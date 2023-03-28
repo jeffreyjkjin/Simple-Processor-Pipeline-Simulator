f002dd755ecc524b298e0d3ab903f954f81f56e7
jeffreyjkjin <93238445+jeffreyjkjin@users.noreply.github.com>
2023-03-27 19:52:19 -0700

    Created instruction class.
    -Added enumerate for the type of instruction
    -Bundled attributes of an instruction into the Instruction class
    -Implemented constructor for Instruction

---
+++ instruction.hpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+#pragma once
+
+using namespace std;
+
+// Type of program instructions.
+typedef enum {
+   Integer = 1,
+   Float = 2,
+   Branch = 3,
+   Load = 4,
+   Store = 5 
+} iType;
+
+// Contains the attributes of a single instruction.
+class Instruction {
+    public:
+        int PC;                 // Base 10 (from hexadecimal) value representing the instruction address.
+        iType type;             // The type of the instruction.
+        vector<int> dependents; // List of PC values of instructions that the current instruction 
+                                // is dependent on.
+
+        //  DESC: Creates a new instruction with the provided parameters.
+        // PARAM: PC - Program counter.
+        //        type - Type of instruction.
+        //        dependents - List of dependencies that the current instruction relies on.
+        Instruction(int PC, iType type, vector<int> dependents): PC(PC), type(type), dependents(dependents) {};
+};
