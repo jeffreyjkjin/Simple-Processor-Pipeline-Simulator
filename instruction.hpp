e7714bffbe7e1e3070201bc19d710daeefc4cf1b
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-03-27 22:42:36 -0700

    Updated Instruction class.
    -Changed PC to string and dependents to a vector of strings
    -Updated constructor parameters to use new attributes types

--- instruction.hpp
+++ instruction.hpp
@@ -16,14 +16,13 @@ typedef enum {
 // Contains the attributes of a single instruction.
 class Instruction {
     public:
-        int PC;                 // Base 10 (from hexadecimal) value representing the instruction address.
-        iType type;             // The type of the instruction.
-        vector<int> dependents; // List of PC values of instructions that the current instruction 
-                                // is dependent on.
+        string PC;                 // Base 10 (from hexadecimal) value representing the instruction address.
+        iType type;                // The type of the instruction.
+        vector<string> dependents; // List of PC values of instructions that the current instruction is dependent on.
 
         //  DESC: Creates a new instruction with the provided parameters.
         // PARAM: PC - Program counter.
         //        type - Type of instruction.
         //        dependents - List of dependencies that the current instruction relies on.
-        Instruction(int PC, iType type, vector<int> dependents): PC(PC), type(type), dependents(dependents) {};
+        Instruction(string PC, iType type, vector<string> dependents): PC(PC), type(type), dependents(dependents) {};
 };
